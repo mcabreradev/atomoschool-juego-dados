bbb9383e03ecb17c86480895470e631d2a47e800
Miguelangel Cabrera <mcabrera.dev@gmail.com>
2022-09-15 19:19:34 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { useState, useCallback } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dado1, setDado1] = useState(0);
+  const [dado2, setDado2] = useState(0);
+  const [isBusy, setBusy] = useState(false);
+
+  const dados = ['dado1.png', 'dado2.png', 'dado3.png', 'dado4.png', 'dado5.png', 'dado6.png']
+
+  const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
+
+  const onClick = useCallback(() => {
+    setBusy(true)
+    setTimeout(() => {
+      setDado1(random(0,5));
+      setDado2(random(0,5));
+      setBusy(false)
+    }, 600);
+  }, []);
+
+  const busyClass = isBusy ? 'rotate': '';
+
+  console.log("render..")
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      
+      <div className="lista-dados">
+        <img src={dados[dado1]} className={ busyClass } />
+        <img src={dados[dado2]} className={ busyClass } />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div className="boton-girar">
+        <button onClick={onClick}>Girar</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
